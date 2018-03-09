81da7026dec4662e633ee42adf79b388df32827c
AmirHossein Ebrahimi <amrihosseinebrahimi77@gmail.com>
2018-03-09 10:32:43 +0330

    Models User

--- alonecoders/models.py
+++ alonecoders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    Username = models.CharField()
+    Email = models.EmailField()
+    Password = models.CharField()
+    Confirm_password = models.CharField()
+    Student_id = models.IntegerField()
+    Name = models.CharField()
+    Major = models.CharField(choices=majors)
+
+    def add_major(self, item_short, item_name):
+        new_item = ((item_short, item_name),)
+        self.Majors = self.Majors + new_item
+
+    def __str__(self):
+        return repr(self.Studen_id)
