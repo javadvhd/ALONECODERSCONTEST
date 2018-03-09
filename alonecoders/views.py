fb84b4ef4d98fb69345dc37a9726c2a6f8c4dd89
AmirHossein Ebrahimi <amrihosseinebrahimi77@gmail.com>
2018-03-09 12:03:48 +0330

    request insted of form replaced

--- alonecoders/views.py
+++ alonecoders/views.py
@@ -6,26 +6,21 @@ from .froms import *
 
 
 def login(request):
+    return render(request, 'login.html')
 
+def save_user(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            Username = form.cleaned_data['Username']
-            Email = form.cleaned_data['Email']
-            Password = form.cleaned_data['Password']
-            Student_id = form.cleaned_data['Student_id']
-            Name = form.cleaned_data['Name']
-            Major = form.cleaned_data['Major']
-
-            User(
-                Username=Username,
-                Password=Password,
-                Email=Email,
-                Student_id=Student_id,
-                Name=Name,
-                Major=Major
-            ).save()
-            return HttpResponse('/tanks/')
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
+        Username = request.POST['Username']
+        Email = request.POST['Email']
+        Password = request.POST['Password']
+        Student_id = request.POST['Student_id']
+        Name = request.POST['Name']
+        Major = request.POST['Major']
+        User(
+            Username=Username,
+            Password=Password,
+            Email=Email,
+            Student_id=Student_id,
+            Name=Name,
+            Major=Major
+        ).save()
