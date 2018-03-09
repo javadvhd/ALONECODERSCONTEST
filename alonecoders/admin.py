03632d1cc4e25a9fc66319a6437c12c79690564c
Iman Karimi <emankariminouri@gmail.com>
2018-03-09 13:24:28 +0330

    fix model: Text, User

--- alonecoders/admin.py
+++ alonecoders/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Text, User, Major
+from .models import Text, User
 
 admin.site.register(Text)
 admin.site.register(User)
-admin.site.register(Major)
--- alonecoders/models.py
+++ alonecoders/models.py
@@ -1,32 +1,23 @@
 from django.db import models
 
 
-class Major(models.Model):
-    def __init__(self):
-        short_m = models.CharField(max_length=2)
-        name_m = models.CharField(max_length=20)
-
-    short_m = models.CharField(max_length=2)
-    name_m = models.CharField(max_length=20)
-
-
-def make_choice():
-    choice = [('CS', 'computer science')]
-    objs = Major.objects.all()
-    if objs:
-        for obj in objs:
-            choice = choice + [(obj.short_m, obj.name_m)]
-    return choice
-
-
 class User(models.Model):
     Username = models.CharField(max_length=50)
     Email = models.EmailField(max_length=100)
     Password = models.CharField(max_length=20)
     Student_id = models.IntegerField()
     Name = models.CharField(max_length=50)
-    last_choices = make_choice()
-    major = models.CharField(max_length=2, choices=last_choices)
+    COMPUTER_ENGINEERING = 'CE'
+    COMPUTER_SCIENCE = 'CS'
+    MAJOR_CHOICES = (
+        (COMPUTER_ENGINEERING, 'computer engineering'),
+        (COMPUTER_SCIENCE, 'computer science'),
+    )
+    major = models.CharField(
+        max_length=2,
+        choices=MAJOR_CHOICES,
+        default=COMPUTER_SCIENCE,
+    )
 
     def str(self):
         return repr(self.Student_id)
